02730baa8f6f0ffb6cf20c62b59439b6f290ed13
Stigh87 <88627426+Stigh87@users.noreply.github.com>
2022-02-07 09:35:27 +0100

    Add files via upload

---
+++ Players/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Players
+{
+    internal class Player
+    {
+        Random Random = new Random();
+        public string Name;
+        public int Poeng;
+
+        public Player(string name, int poeng)
+        {
+            Name = name;
+            Poeng = poeng;
+        }
+
+        public void Play(Player player2)
+        {
+            var winnerIndex = Random.Next(0, 1);
+
+            if (winnerIndex == 0)
+            {
+                Poeng++;
+                player2.Poeng--;
+                Console.WriteLine(Name + " vant runden over " + player2.Name);
+            }
+            else
+            {
+                player2.Poeng++;
+                Poeng--;
+                Console.WriteLine(player2.Name + " vant runden over " + Name);
+            }
+
+        }
+
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine(Name + " Har " + Poeng + " poeng");
+
+        }
+    }
+}
---
+++ Players/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Players
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var players = new[]
+            {
+                new Player("Per", 10), 
+                new Player("Pål", 10), 
+                new Player("Espen", 10)
+            };
+
+
+            for (var round = 1; round <= 10; round++)
+            {
+                var playerIndex1 = random.Next(players.Length);
+                var playerIndex2 = (playerIndex1 + 1 + random.Next(2)) % players.Length;
+                var player1 = players[playerIndex1];
+                var player2 = players[playerIndex2];
+                player1.Play(player2);
+            }
+            Console.WriteLine();
+
+            var i = 0;
+            foreach (var player in players)
+            {
+                player.ShowNameAndPoints();
+                if (i == 2)
+                {
+                    string winningPlayer;
+                    if (players[0].Poeng > players[1].Poeng && players[0].Poeng > players[2].Poeng)
+                    {
+                        winningPlayer = players[0].Name;
+                        PrintWinner(winningPlayer);
+                    }
+                    else if (players[1].Poeng > players[2].Poeng && players[1].Poeng > players[0].Poeng)
+                    {
+                        winningPlayer = players[1].Name;
+                        PrintWinner(winningPlayer);
+                    }
+                    else
+                    {
+                        winningPlayer = players[2].Name;
+                        PrintWinner(winningPlayer);
+                    }
+                }
+                i++;
+            }
+
+        }
+
+        private static void PrintWinner(string winningPlayer)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Vinneren er: " + winningPlayer);
+        }
+    }
+}
